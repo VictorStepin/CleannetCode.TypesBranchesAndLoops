8733988650519ead4a22179931875330dee642cd
Victor Stepin <vicstep1994@gmail.com>
2022-02-15 23:03:33 +0700

    Guess game implemented.

--- CleannetCode.TypesBranchesAndLoops/CleannetCode.GuessGame/Program.cs
+++ CleannetCode.TypesBranchesAndLoops/CleannetCode.GuessGame/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Добро пожаловать в игру \"Угадай число\"!");
+Console.WriteLine();
+Console.WriteLine("Нажмите любую клавишу, чтобы начать игру.");
+Console.ReadKey();
+
+Console.Clear();
+int minHiddenNumber = 0;
+int maxHiddenNumber = 999;
+int hiddenNumber = new Random().Next(minHiddenNumber, maxHiddenNumber + 1);
+Console.WriteLine($"Загадано число от {minHiddenNumber} до {maxHiddenNumber}. Вам необходимо угадать число.");
+Console.Write("Введите число и нажмите Enter: ");
+
+int attemptCount = 0;
+bool hiddenNumberGuessed = false;
+string? userInput;
+while (!hiddenNumberGuessed)
+{
+    userInput = Console.ReadLine();
+    int attempt = 0;
+    Console.WriteLine();
+    if (int.TryParse(userInput, out attempt))
+    {
+        if (attempt < minHiddenNumber || attempt > maxHiddenNumber)
+        {
+            Console.Write($"Введенное число вне диапазона от {minHiddenNumber} до {maxHiddenNumber}. Попробуйте еще раз: ");
+        }
+        else
+        {
+            attemptCount++;
+            if (attempt == hiddenNumber)
+            {
+                hiddenNumberGuessed = true;
+            }
+            else if (attempt < hiddenNumber)
+            {
+                Console.Write($"Ваша попытка: {attempt}. Загаданное число больше. Попробуйте еще раз: ");
+            }
+            else
+            {
+                Console.Write($"Ваша попытка: {attempt}. Загаданное число меньше. Попробуйте еще раз: ");
+            }
+        }
+    }
+    else
+    {
+        Console.Write("Некорректный ввод. Попробуйте еще раз: ");
+    }
+}
+
+Console.Clear();
+Console.WriteLine($"Поздравляем! Вы угадали число! Количество попыток: {attemptCount}.");
